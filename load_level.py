4edce1e87cdff7a4b05fd674586ea0904018448a
EluciferE <45201676+EluciferE@users.noreply.github.com>
2019-02-10 22:06:05 +0500

    load_level working verssion
    Текст находится с питоновским файлом. Пример текста:
    Background-level_test_fon.jpg
    Platform-0 550 1024 50
    Platform-24 350 300 25
    Platform-700 350 300 25
    Platform-370 200 300 25

--- load_level.py
+++ load_level.py
@@ -1,9 +1,4 @@
-import os
-
-
-
 def load_level(filename):
-    filename = 'data/' + filename
     file = open(filename, 'r')
     elements = file.read()
     elements = elements.split('\n')
@@ -12,17 +7,12 @@ def load_level(filename):
         type = element[0]
         if type == 'Platform':
             element = element[1].split()
-            pos = (element[1], element[2])
-            scale = (element[3], element[4])
+            pos = (int(element[0]), int(element[1]))
+            scale = (int(element[2]), int(element[3]))
             Platform((pos), (scale))
-        if type == 'Player':
-            element = element[1].split()
-            Player(element[0], element[1])
         if type == 'Background':
             name = element[1]
             Background = pygame.sprite.Sprite()
             Background.image = pygame.transform.scale(load_image(name), (1024, 550))
             Background.rect = Background.image.get_rect()
             level_sprites.add(Background)
-
-load_level('level1.txt')
